a52c85140e67aeea381167cf9b1e70eab1c273e0
Alex Bauman <alex.bauman117@gmail.com>
2016-10-24 02:50:49 -0400

    Refactored Connect and Login into Promises

--- test/test.js
+++ test/test.js
@@ -4,11 +4,42 @@ var _vndb = require('../vndb.js');
 var vndb = new _vndb("vndbtestprocess");
 
 describe("vndb.query", function() {
-    it("should return JSON object of results", function() {
-        vndb.query("dbstats").then( (resolve) => {
+    it("Should succeed and return JSON object of results", function() {
+        vndb.query("dbstats").then( function(resolve) {
             resolve.should.be.a('object');
             resolve.should.have.property('users');
             resolve.should.have.length(9);
         });
     })
+    it("Should fail and do stuff", function() {
+        vndb.query("get vn fd").then( function(resolve) {
+        }, function(reject) {
+            //console.log(reject);
+        })
+    })
+    it("Should fail and return 'invalid query'", function() {
+        vndb.query(false).then( function(resolve) {
+        }, function(reject) {
+            reject.should.be.a('string');
+            reject.should.equal('Invalid query.');
+        });
+    })
 });
+
+describe("vndb.get", function() {
+    it("Should suceed and return details on a VN", function() {
+        vndb.get({type:"vn", flags:["basic", "details"], filter:{type:"id", oper:"=", value:"17"}}).then( function(resolve) {
+            resolve.should.be.a('object');
+            resolve.should.have.property('title');
+            resolve.should.have.property('description');
+            resolve.should.have.property('rating');
+        });
+    })
+    it("Should fail and return 'invalid query'", function() {
+        vndb.get({}).then( function(resolve) {
+        }, function(reject) {
+            reject.should.be.a('string');
+            reject.should.equal('Invalid query.');
+        });
+    })
+})
--- vndb.js
+++ vndb.js
@@ -1,4 +1,6 @@
 var net = require('net');
+var emitter = require('events');
+var status = new emitter();
 var version = require('./package.json').version
 var client = new net.Socket();
 
@@ -8,58 +10,69 @@ class Api {
         startListeners();
         this.connected = false;
         this.logged = false;
+        loginListener();
+    }
+
+    get(args) {
+        var message = parseArgs(args);
+        return this.query(`get ${message}`);
+
     }
 
     query(message) {
         return new Promise( (resolve, reject) => {
-            var chunk = "";
-            client.on('data', (data) => {
-                chunk += data.toString();
-                if (data.indexOf("\x04") === -1) {
-                    return;
-                }
-                let extracted = chunk.substring(0, chunk.indexOf("\x04"));
-                chunk = "";
-                if (extracted === "ok") {
-                    console.log("Login Successful")
-                    this.logged = true;
-                } else {
-                    let command = extracted.substring(0, extracted.indexOf(" "));
-                    extracted = extracted.replace(`${command} `, "");
-                    let output = JSON.parse(extracted).items;
-                    console.log(output);
+            if (message === false) {
+                return reject("Invalid query.")
+            }
+            status.emit('login')
+            status.on('ready', () => {
+                client.on('error', (error) => {
+                    console.log(`ERROR: ${error}`);
+                });
+                var chunk = "";
+                client.on('data', (data) => {
+                    chunk += data.toString();
+                    if (data.indexOf("\x04") === -1) {
+                        return;
+                    }
+                    let response = chunk.substring(0, chunk.indexOf("\x04"));
+                    chunk = chunk.replace(`${response}\x04`, "")
+                    let command = response.substring(0, response.indexOf(" "));
+                    let json = response.replace(`${command} `, "");
+                    let output = JSON.parse(json);
                     client.removeAllListeners('data');
-                    return resolve(output)
-                }
-            });
-            if (this.connected === false) {
-                connect();
-            }
-            if (this.logged === false) {
-                login();
-            }
-            client.write(`${message}\x04`);
+                    if (command === "error") {
+                        return reject(`${command}: ${output.msg}`)
+                    } else if (command === "results" || command === "dbstats") {
+                        return resolve(output)
+                    }
+                });
+                client.write(`${message}\x04`);
+            })
         })
     }
 }
 
+function loginListener() {
+    status.once('login', () => {
+        connect().then( (result) => {
+            login().then( (result) => {
+                status.emit('ready');
+            }, (reject) => {
+                console.log("Login failed.")
+            })
+        }, (reject) => {
+            console.log("Connection failed.")
+        })
+    })
+}
+
 function startListeners() {
-    client.on('connect', () => {
-        console.log('Connection successful');
-        this.connected = true;
-        client.setTimeout(60000)
-        client.setEncoding("utf8");
-    });
-
-    client.on('error', (error) => {
-        console.log(`ERROR: ${error}`);
-        client.destroy();
-    });
-
     client.on('close', () => {
         console.log("Connection closed.");
         this.connected = false;
         this.logged = false;
+        loginListener();
     });
 
     client.on('timeout', () => {
@@ -69,12 +82,52 @@ function startListeners() {
     });
 }
 
+function parseArgs(args) {
+    var type = args.hasOwnProperty("type") ? args.type : false
+    var flags = args.hasOwnProperty("flags") ? args.flags : ["basic", "details", "stats"];
+    var filter = args.hasOwnProperty("filter") ? args.filter : false;
+    if (type === false || filter === false) { return false};
+    return `${args.type} ${flags.join(',')} (${filter.type} ${filter.oper} ${filter.value})`
+}
+
 function connect() {
-    client.connect(19534, "api.vndb.org");
+    return new Promise( (resolve, reject) => {
+        client.on('error', (error) => {
+            console.log(`ERROR: ${error}`);
+            return reject();
+        });
+        client.on('connect', () => {
+            console.log("connection successful");
+            this.connected = true;
+            client.setTimeout(60000)
+            client.setEncoding("utf8");
+            client.removeAllListeners('error');
+            client.removeAllListeners('connect');
+            return resolve();
+        })
+        client.connect(19534, "api.vndb.org");
+    })
 }
 
 function login() {
-    client.write(`login {"protocol":1,"client":"vndbjs","clientver":"${version}"}\x04`)
+    return new Promise( (resolve, reject) => {
+        client.on('error', (error) => {
+            console.log(`ERROR: ${error}`);
+        });
+        client.on('data', (data) => {
+            let chunk = "";
+            chunk += data.toString();
+            let response = chunk.substring(0, chunk.indexOf("\x04"));
+            if (response === "ok") {
+                console.log("Logged in to VNDB")
+            }
+            client.removeAllListeners('error');
+            client.removeAllListeners('data');
+            return resolve();
+        })
+        client.write(`login {"protocol":1,"client":"${this.clientName}","clientver":"${version}"}\x04`)
+    })
 }
+
 
 module.exports = Api;
