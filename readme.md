5a3012fbf60aadb73425f0bf7c646fd00fa68d05
Arbauman <arbauman207@gmail.com>
2016-10-25 00:51:01 -0400

    Connection Pooling
    Added a dynamic connection pool
    Added API wrapper functions
    Updated Readme
    Updated tests

--- readme.md
+++ readme.md
@@ -12,23 +12,105 @@ A Node.js library for accessing the VNDB.org database.
 vndbjs currently does not support logging in to the database with a VNDB account.  This functionality is required for making changes to the database.  This functionality may be included at a later date, but is not planned at the moment.
 
 ## Features
-* Takes care of connecting and logging in.  
+* Takes care of connecting and logging in.
+* Sets up a pool of connections (up to 10, as allowed by VNDB) and allocates requests automatically.
 * All queries are returned as a Promise.
 * Database results are returned as JSON objects for easy parsing.
 
-## Getting Started
+## Installation
 `npm install --save vndbjs`
 
-## Documentation
+## Usage
 ```js
 var _vndb = require('vndbjs');
-var vndb = new _vndb("<client name>");
+var vndb = new _vndb("<client name>"); // Unique name for your client program
 
-vndb.query(`get vn basic,details,stats (title = "Muv-Luv")`).then( (output) => {
-    console.log(output); // Output is a JSON object of database data
-    console.log(output.title) // "Muv-Luv"
-    console.log(output.original) // "マブラヴ"
+vndb.getVnByTitle("Ever17 - The Out of Infinity-").then( (output) => {
+    /**{ items:
+       [ { languages: [Object],
+           image: 'https://s.vndb.org/cv/60/24260.jpg',
+           orig_lang: [Object],
+           rating: 8.66,
+           links: [Object],
+           image_nsfw: false,
+           title: 'Ever17 -The Out of Infinity-',
+           votecount: 4345,
+           length: 4,
+           description: 'Ever17 is the tale of seven individuals who come to be trapped 153 feet within the underwater marine theme park, LeMU. During an apparently normal day at the park, a massive accident happens, placing almost half of the facility underwater. The path to the surface and the communication lines are cut off. In addition, LeMU is under constant assault by severe water pressure, limiting time to find a means of escape to 119 hours. Escape is not the only concern, however; many questions arise as to the legitimacy of the accident and whether or not those trapped there were brought there for a purpose.\n\n[From [url=http://en.wikipedia.org/wiki/Ever_17:_The_Out_of_Infinity]Wikipedia[/url]]',
+           aliases: 'Ever 17\nEbaa Sebuntiin\nエバー・セブンティーン\ne17',
+           platforms: [Object],
+           original: null,
+           popularity: 63.5,
+           released: '2002-08-29',
+           id: 17 } ],
+      more: false,
+      num: 1 }**/
 });
 ```
+
+# Documentation
+## Templates
+These are simplified wrappers designed to provide easy access to the most common queries one might need.  If you have an idea for an additional template, open an issue.
+
+### dbstats()
+Arguments: None
+
+Returns: JSON object of various stats for vndb.org
+
+### searchVnList(string)
+Arguments: String, punctuation and spelling errors won't result in errors.
+
+Returns: JSON object of matches.
+
+### getVnByTitleFull(string)
+Arguments: String, punctuation and spelling must match exactly.
+
+Returns: JSON object of all data available for that entry.
+
+### getVnByTitle(string)
+Arguments: String, punctuation and spelling must match exactly.
+
+Returns: JSON object of basic, details, and stats for that entry.
+
+### getVnByIdFull(int)
+Arguments: Integer
+
+Returns: JSON object of all data available for that entry.
+
+### getVnById(int)
+Arguments: Integer
+
+Returns: JSON object of basic, details, and stats for that entry.
+
+## Custom Searches
+If none of the above functions achieve quite what you want, these functions allow you to request specific results.
+
+### get({options})
+This function allows you to pass in an object, which must be formatted as below.  All fields are mandatory except for flags, which will default to `["basic", "details", "stats"]` if none are provided.  
+
+*Note: At this time get() does not yet support the "options" argument, which allows setting values for `page`, `results`, `sort`, and `reverse`.*
+```js
+get({
+    type: vn,
+    flags: ["basic", "details"],
+    filter: {
+        type: "search",
+        oper: "~",
+        value: "muv-luv"
+    }
+})
+```
+
+### query(string)
+This is the most basic function, allowing you to input the string which is sent to VNDB.org directly.  
+```js
+query('get vn basic,details (search ~ "muv-luv")')
+```
+Since this option is the simplest and easily customized, it should allow you to request any information accessible through VNDB's API.  Consult [VNDB's documentation](https://vndb.org/d11) for more information on how to build your strings.
+
+*Note: The quotes around the strings in the filter section (in this case, "muv-luv") are **necessary**.  Don't forget them, or you'll get an error.*
+
+
+
 ## License
 vndbjs is licensed under the [MIT](license) license.  This library is not associated with VNDB.org
--- vndb.js
+++ vndb.js
@@ -1,85 +1,95 @@
 var net = require('net');
-var emitter = require('events');
-var status = new emitter();
 var version = require('./package.json').version
-var client = new net.Socket();
+var shortid = require('shortid');
+var Pool = require('generic-pool').Pool;
 
 class Api {
+
     constructor(clientName) {
         this.clientName = clientName;
-        startListeners();
-        this.connected = false;
-        this.logged = false;
-        loginListener();
+        this.pool = new Pool({
+            name     : "vndb",
+            create   : function(callback) {
+                var client = new net.Socket();
+                connect(client).then( (result) => {
+                    login(client).then( (result) => {
+                        callback(null, client);
+                    }, (err) => {
+                        console.log("Login failed");
+                    })
+                }, (err) => {
+                    console.log("Connection Failed" + err);
+                })
+            },
+            destroy  : function(client) { client.destroy(); },
+            max      : 10,
+            min      : 2,
+            idleTimeoutMillis : 30000,
+            log : false
+        });
+    }
+    // returns database stats
+    stats() {
+        return this.query('dbstats');
+    }
+    // returns top 10 results for the title query, basic data only
+    searchVnList(title) {
+        return this.query(`get vn basic (search ~ "${title}")`)
+    }
+    // returns all data on title query.  Must be *exact* match.
+    getVnByTitleFull(exactTitle) {
+        return this.query(`get vn basic,details,stats,anime,relations,tags,screens (title = "${exactTitle}")`)
+    }
+    // returns moderate data on title query.  Must be *exact* match.
+    getVnByTitle(exactTitle) {
+        return this.query(`get vn basic,details,stats (title = "${exactTitle}")`)
+    }
+    // returns all data on id query.
+    getVnByIdFull(id) {
+        return this.query(`get vn basic,details,stats,anime,relations,tags,screens (id = ${id})`)
+    }
+    // returns moderate data on id query.
+    getVnById(id) {
+        return this.query(`get vn basic,details,stats (id = ${id})`)
     }
 
     get(args) {
         var message = parseArgs(args);
         return this.query(`get ${message}`);
-
     }
 
     query(message) {
         return new Promise( (resolve, reject) => {
-            if (message === false) {
-                return reject("Invalid query.")
-            }
-            status.emit('login')
-            status.on('ready', () => {
-                client.on('error', (error) => {
-                    console.log(`ERROR: ${error}`);
-                });
-                var chunk = "";
-                client.on('data', (data) => {
-                    chunk += data.toString();
-                    if (data.indexOf("\x04") === -1) {
-                        return;
-                    }
-                    let response = chunk.substring(0, chunk.indexOf("\x04"));
-                    chunk = chunk.replace(`${response}\x04`, "")
-                    let command = response.substring(0, response.indexOf(" "));
-                    let json = response.replace(`${command} `, "");
-                    let output = JSON.parse(json);
-                    client.removeAllListeners('data');
-                    if (command === "error") {
-                        return reject(`${command}: ${output.msg}`)
-                    } else if (command === "results" || command === "dbstats") {
-                        return resolve(output)
-                    }
-                });
-                client.write(`${message}\x04`);
-            })
+            this.pool.acquire( (err, client) => {
+                if (err) {
+                    console.log(`ERROR: ${err}`);
+                }
+                else {
+                    var chunk = "";
+                    client.on('data', (data) => {
+                        chunk += data.toString();
+                        if (data.indexOf("\x04") === -1) {
+                            return;
+                        }
+                        let response = chunk.substring(0, chunk.indexOf("\x04"));
+                        chunk = chunk.replace(`${response}\x04`, "")
+                        let command = response.substring(0, response.indexOf(" "));
+                        let json = response.replace(`${command} `, "");
+                        let output = JSON.parse(json);
+                        client.removeAllListeners('data');
+                        if (command === "error") {
+                            this.pool.release(client);
+                            return reject(response);
+                        } else if (command === "results" || command === "dbstats") {
+                            this.pool.release(client);
+                            return resolve(output)
+                        }
+                    });
+                    client.write(`${message}\x04`);
+                }
+            });
         })
     }
-}
-
-function loginListener() {
-    status.once('login', () => {
-        connect().then( (result) => {
-            login().then( (result) => {
-                status.emit('ready');
-            }, (reject) => {
-                console.log("Login failed.")
-            })
-        }, (reject) => {
-            console.log("Connection failed.")
-        })
-    })
-}
-
-function startListeners() {
-    client.on('close', () => {
-        console.log("Connection closed.");
-        this.connected = false;
-        this.logged = false;
-        loginListener();
-    });
-
-    client.on('timeout', () => {
-        this.connected = false;
-        this.logged = false;
-        client.destroy();
-    });
 }
 
 function parseArgs(args) {
@@ -90,26 +100,7 @@ function parseArgs(args) {
     return `${args.type} ${flags.join(',')} (${filter.type} ${filter.oper} ${filter.value})`
 }
 
-function connect() {
-    return new Promise( (resolve, reject) => {
-        client.on('error', (error) => {
-            console.log(`ERROR: ${error}`);
-            return reject();
-        });
-        client.on('connect', () => {
-            console.log("connection successful");
-            this.connected = true;
-            client.setTimeout(60000)
-            client.setEncoding("utf8");
-            client.removeAllListeners('error');
-            client.removeAllListeners('connect');
-            return resolve();
-        })
-        client.connect(19534, "api.vndb.org");
-    })
-}
-
-function login() {
+function login(client) {
     return new Promise( (resolve, reject) => {
         client.on('error', (error) => {
             console.log(`ERROR: ${error}`);
@@ -119,15 +110,31 @@ function login() {
             chunk += data.toString();
             let response = chunk.substring(0, chunk.indexOf("\x04"));
             if (response === "ok") {
-                console.log("Logged in to VNDB")
+                //console.log("Logged in to VNDB")
             }
             client.removeAllListeners('error');
             client.removeAllListeners('data');
             return resolve();
         })
-        client.write(`login {"protocol":1,"client":"${this.clientName}","clientver":"${version}"}\x04`)
+        client.write(`login {"protocol":1,"client":"${this.clientName}-${shortid.generate()}","clientver":"${version}"}\x04`)
     })
 }
 
+function connect(client) {
+    return new Promise( (resolve, reject) => {
+        client.on('error', (error) => {
+            console.log(`ERROR: ${error}`);
+            return reject();
+        });
+        client.on('connect', () => {
+            //console.log("connection successful");
+            client.setEncoding("utf8");
+            client.removeAllListeners('error');
+            client.removeAllListeners('connect');
+            return resolve();
+        })
+        client.connect(19534, "api.vndb.org");
+    })
+}
 
 module.exports = Api;
