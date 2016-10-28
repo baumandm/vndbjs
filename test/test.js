00196555cd285ba4cba47f9c6aca49bae877ece1
Dave Bauman <baumandm@gmail.com>
2016-10-28 00:54:47 -0400

    Fix unit tests

--- test/test.js
+++ test/test.js
@@ -1,106 +1,149 @@
 var should = require('chai').should();
 
 var _vndb = require('../vndb.js');
-var vndb = new _vndb("vndbtestprocess");
+var vndb = new _vndb("vndbtestprocess", {});
 
 describe("query", function() {
-    it("Should succeed and return JSON object of results", function() {
-        vndb.query("dbstats").then( function(resolve) {
-            resolve.should.be.a('object');
-            resolve.should.have.property('users');
-            resolve.should.have.length(9);
-        });
+  this.timeout(5000);
+  it("Should succeed and return JSON object of results", function(done) {
+    vndb.query("dbstats").then( function(resolve) {
+      resolve.should.be.a('object');
+      resolve.should.have.property('users');
+      done();
+    }).catch(function (error) {
+      done(error);
     });
-    it("Should fail and do stuff", function() {
-        vndb.query("get vn fd").then( function(resolve) {
-        }, function(reject) {
-            reject.should.be.a('string');
-            reject.should.equal('error');
-        });
+  });
+  it("Should fail and do stuff", function(done) {
+    vndb.query("get vn fd").then( function(resolve) {
+      done("Expected an error");
+    }, function(reject) {
+      reject.should.be.a('string');
+      //reject.should.equal('error');
+      done();
     });
+  });
 });
 
 describe("get", function() {
-    it("Should succeed and return details on a VN", function() {
-        vndb.get({type:"vn", flags:["basic", "details"], filter:{type:"id", oper:"=", value:"17"}}).then( function(resolve) {
-            resolve.should.be.a('object');
-            resolve.should.have.property('title');
-            resolve.should.have.property('description');
-            resolve.should.have.property('rating');
-        });
+  it("Should succeed and return details on a VN", function(done) {
+    vndb.get({type:"vn", flags:["basic", "details"], filter:{type:"id", oper:"=", value:"17"}}).then( function(resolve) {
+      resolve.should.be.a('object');
+      resolve.should.have.property('items');
+      resolve.items.should.have.length(1);
+      let item = resolve.items[0];
+      item.should.have.property('title');
+      item.should.have.property('description');
+      item.should.not.have.property('rating');
+      done();
+    }).catch(function(error) {
+      done(error);
     });
-    it("Should fail and return 'invalid query'", function() {
-        vndb.get({}).then( function(resolve) {
-        }, function(reject) {
-            reject.should.be.a('string');
-            reject.should.equal('Invalid query.');
-        });
+  });
+  it("Should fail and return 'invalid query'", function(done) {
+    vndb.get({}).then( function(resolve) {
+      done("Expected an error");
+    }, function(reject) {
+      reject.should.be.a('string');
+      //reject.should.equal('Invalid query.');
+      done();
     });
+  });
 });
 
 describe("stats", function() {
-    it("Should succeed and return JSON", function() {
-        vndb.stats().then( function(resolve) {
-            resolve.should.be.a('object');
-            resolve.should.have.property('users');
-            resolve.should.have.property('vn');
-            resolve.should.have.property('chars');
-        });
+  it("Should succeed and return JSON", function(done) {
+    vndb.stats().then( function(resolve) {
+      resolve.should.be.a('object');
+      resolve.should.have.property('users');
+      resolve.should.have.property('vn');
+      resolve.should.have.property('chars');
+      done();
+    }).catch(function(error) {
+      done(error);
     });
+  });
 });
 
 describe("searchVnList", function() {
-    it("Should succeed and return JSON", function() {
-        vndb.searchVnList("muv luv").then( function(resolve) {
-            resolve.should.be.a('object');
-            resolve.should.have.property('num');
-            resolve.should.have.property('more');
-            resolve.should.have.property('items');
-            resolve.items[0].should.have.property('title');
-        });
+  it("Should succeed and return JSON", function(done) {
+    vndb.searchVnList("muv luv").then( function(resolve) {
+      resolve.should.be.a('object');
+      resolve.should.have.property('num');
+      resolve.should.have.property('more');
+      resolve.should.have.property('items');
+      resolve.items[0].should.have.property('title');
+      done();
+    }).catch(function(error) {
+      done(error);
     });
+  });
 });
 
 describe("getVnByTitleFull", function() {
-    it("Should succeed and return JSON", function() {
-        vndb.getVnByTitleFull("Muv-Luv").then( function(resolve) {
-            resolve.should.be.a('object');
-            resolve.should.have.property('tags');
-            resolve.should.have.property('relations');
-            resolve.should.have.property('anime');
-        });
+  it("Should succeed and return JSON", function(done) {
+    vndb.getVnByTitleFull("Muv-Luv").then( function(resolve) {
+      resolve.should.be.a('object');
+      resolve.should.have.property('items');
+      resolve.items.should.have.length(1);
+      let item = resolve.items[0];
+      item.should.have.property('tags');
+      item.should.have.property('relations');
+      item.should.have.property('anime');
+      done()
+    }).catch(function(error) {
+      done(error);
     });
+  });
 });
 
 describe("getVnByTitle", function() {
-    it("Should succeed and return JSON", function() {
-        vndb.getVnByTitle("Muv-Luv").then( function(resolve) {
-            resolve.should.be.a('object');
-            resolve.should.have.property('image');
-            resolve.should.have.property('rating');
-            resolve.should.have.property('length');
-        });
+  it("Should succeed and return JSON", function(done) {
+    vndb.getVnByTitle("Muv-Luv").then( function(resolve) {
+      resolve.should.be.a('object');
+      resolve.should.have.property('items');
+      resolve.items.should.have.length(1);
+      let item = resolve.items[0];
+      item.should.have.property('image');
+      item.should.have.property('rating');
+      item.should.have.property('length');
+      done();
+    }).catch(function(error) {
+      done(error);
     });
-})
-;
+  });
+});
+
 describe("getVnByIdFull", function() {
-    it("Should succeed and return JSON", function() {
-        vndb.getVnByIdFull(17).then( function(resolve) {
-            resolve.should.be.a('object');
-            resolve.should.have.property('tags');
-            resolve.should.have.property('relations');
-            resolve.should.have.property('anime');
-        });
+  it("Should succeed and return JSON", function(done) {
+    vndb.getVnByIdFull(17).then( function(resolve) {
+      resolve.should.be.a('object');
+      resolve.should.have.property('items');
+      resolve.items.should.have.length(1);
+      let item = resolve.items[0];
+      item.should.have.property('tags');
+      item.should.have.property('relations');
+      item.should.have.property('anime');
+      done();
+    }).catch(function(error) {
+      done(error);
     });
+  });
 });
 
 describe("getVnById", function() {
-    it("Should succeed and return JSON", function() {
-        vndb.getVnById(17).then( function(resolve) {
-            resolve.should.be.a('object');
-            resolve.should.have.property('image');
-            resolve.should.have.property('rating');
-            resolve.should.have.property('length');
-        });
+  it("Should succeed and return JSON", function(done) {
+    vndb.getVnById(17).then( function(resolve) {
+      resolve.should.be.a('object');
+      resolve.should.have.property('items');
+      resolve.items.should.have.length(1);
+      let item = resolve.items[0];
+      item.should.have.property('image');
+      item.should.have.property('rating');
+      item.should.have.property('length');
+      done();
+    }).catch(function(error) {
+      done(error);
     });
+  });
 });
